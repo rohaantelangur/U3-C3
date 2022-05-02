63b4491ccbdf21f92033663a6246792b77a72486
rohaantelangur <rohaantelangur@gmail.com>
2022-05-02 12:41:22 +0530

    U3-C4

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,13 @@
 // Ude Import export (MANDATORY)
 // Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+import { navbar, searchNews, searchbycounty } from "../components/AllFunction.js"
+  document.getElementById("navbar").innerHTML=navbar()
+  // console.log(navbar());
+
+document.getElementById("search_input").addEventListener("input", searchNews)
+
+let countries = document.getElementById("sidebar").children
+// console.log(countries);
+for(let element of countries){
+  element.addEventListener("click",searchbycounty)
+}
